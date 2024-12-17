f1d705b28422f93bf3f7410511e940ccd7d9c2f5
RihoMochizuki <r.mochizuki@rhino-inc.jp>
2024-12-17 16:16:21 +0900

    クラス名修正、Lintチェックエラー解消

--- src/js/common.js
+++ src/js/common.js
@@ -1,35 +1,27 @@
-const body = document.body;
-
 const setSlider = () => {
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
+  new Swiper('.swiper', {
     loop: true,
     speed: 600,
-  
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
-      clickable: true
+      clickable: true,
     },
-  
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
   });
-}
+};
 
 const elements = document.querySelectorAll('.c-points-list__info');
 const triggerAnimation = () => {
   elements.forEach((element) => {
     if (!element.classList.contains('animate')) {
-      element.classList.add('animate'); 
+      element.classList.add('animate');
 
       setTimeout(() => {
         element.classList.remove('animate');
-      }, 3800); 
+      }, 3800);
     }
   });
 };
@@ -39,24 +31,26 @@ const initializeKokuchiFlow = () => {
   const pattern2 = document.getElementById('kokuchiPattern2');
   const pattern3 = document.getElementById('kokuchiPattern3');
   const pattern4 = document.getElementById('kokuchiPattern4');
-  
+
   const buttons = document.querySelectorAll('.c-kokuchi-question__btnbox-btn');
 
-  let currentStep1Answer = ''; 
-  let currentStep2Answer = ''; 
+  /* eslint-disable no-unused-vars */
+  let currentStep1Answer = null;
+  let currentStep2Answer = null;
+  /* eslint-enable no-unused-vars */
 
   const resetDisplay = () => {
-    [pattern1, pattern2, pattern3, pattern4].forEach(pattern => {
+    [pattern1, pattern2, pattern3, pattern4].forEach((pattern) => {
       pattern.style.display = 'none';
     });
-  }
+  };
 
   const highlightButton = (clickedButton, parentElement) => {
     const buttonsInQuestion = parentElement.querySelectorAll('.c-kokuchi-question__btnbox-btn');
-    buttonsInQuestion.forEach(button => button.classList.remove('active'));
+    buttonsInQuestion.forEach((button) => button.classList.remove('active'));
     clickedButton.classList.add('active');
-  }
-  
+  };
+
   const handleButtonClick = (event) => {
     const clickedButton = event.target;
     const answer = clickedButton.textContent.trim();
@@ -64,7 +58,7 @@ const initializeKokuchiFlow = () => {
     const step = clickedButton.getAttribute('data-step');
 
     highlightButton(clickedButton, parentElement);
-  
+
     if (step === '1') {
       // 1問目
       currentStep1Answer = answer;
@@ -75,25 +69,30 @@ const initializeKokuchiFlow = () => {
         pattern2.style.display = 'block';
       }
       const step2Buttons = document.querySelectorAll('[data-step="2"]');
-      step2Buttons.forEach(button => button.classList.remove('active')); // 2問目のボタンからactiveを外す
+      step2Buttons.forEach((button) => button.classList.remove('active')); // 2問目のボタンからactiveを外す
     } else if (step === '2') {
       // 2問目
       currentStep2Answer = answer;
       resetDisplay();
       if (answer === 'はい') {
-        pattern3.style.display = 'block'
+        pattern3.style.display = 'block';
       } else {
         pattern4.style.display = 'block';
       }
       pattern2.style.display = 'block';
     }
   };
-  
-  buttons.forEach(button => button.addEventListener('click', (e) => handleButtonClick(e)));
-}
 
-window.addEventListener('load', function() {
-  body.classList.add('loaded');
+  buttons.forEach((button) => button.addEventListener('click', (e) => handleButtonClick(e)));
+};
+
+window.addEventListener('load', function () {
+  const mvElement = document.querySelectorAll(
+    '.section-hero__titleline-pop, .section-hero__titleline-pop-elm, .section-hero__titleline-title, .section-hero__titleline-title-flash-elm, .section-hero__titleline-title-flash-elm--1, .section-hero__titleline-title-flash-elm--2, .section-hero__titleline-title-flash-elm--3, .section-hero__titleline-title-flash-elm-img'
+  );
+  mvElement.forEach(function(element) {
+    element.classList.add('loaded');
+  });
   setSlider();
   triggerAnimation();
   setInterval(triggerAnimation, 5000);
