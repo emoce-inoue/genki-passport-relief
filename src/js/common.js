51056699c7ffc54888fa49bca7ac1ba952840e92
RihoMochizuki <r.mochizuki@rhino-inc.jp>
2024-12-18 16:45:53 +0900

    レビュー対応

--- src/js/common.js
+++ src/js/common.js
@@ -15,26 +15,27 @@ const setSlider = () => {
   }
 };
 
-const elements = document.querySelectorAll('.c-points-list__info');
+const pointsInfoElms = document.querySelectorAll('.c-points-list__info');
 const triggerAnimation = () => {
-  elements.forEach((element) => {
-    if (!element.classList.contains('animate')) {
-      element.classList.add('animate');
+  pointsInfoElms.forEach((pointsInfoElm) => {
+    if (!pointsInfoElm.classList.contains('c-points-list__info--animate')) {
+      pointsInfoElm.classList.add('c-points-list__info--animate');
 
       setTimeout(() => {
-        element.classList.remove('animate');
+        pointsInfoElm.classList.remove('c-points-list__info--animate');
       }, 3800);
     }
   });
 };
 
 const initializeKokuchiFlow = () => {
+  const patterns = document.querySelectorAll('.c-kokuchi-pattern');
   const pattern1 = document.getElementById('kokuchiPattern1');
   const pattern2 = document.getElementById('kokuchiPattern2');
   const pattern3 = document.getElementById('kokuchiPattern3');
   const pattern4 = document.getElementById('kokuchiPattern4');
 
-  const buttons = document.querySelectorAll('.c-kokuchi-question__btnbox-btn');
+  const buttons = document.querySelectorAll('.c-kokuchi-question__button');
 
   /* eslint-disable no-unused-vars */
   let currentStep1Answer = null;
@@ -42,15 +43,15 @@ const initializeKokuchiFlow = () => {
   /* eslint-enable no-unused-vars */
 
   const resetDisplay = () => {
-    [pattern1, pattern2, pattern3, pattern4].forEach((pattern) => {
-      pattern.style.display = 'none';
+    patterns.forEach((pattern) => {
+      pattern.classList.remove('c-kokuchi-pattern--current');
     });
   };
 
   const highlightButton = (clickedButton, parentElement) => {
-    const buttonsInQuestion = parentElement.querySelectorAll('.c-kokuchi-question__btnbox-btn');
-    buttonsInQuestion.forEach((button) => button.classList.remove('active'));
-    clickedButton.classList.add('active');
+    const buttonsInQuestion = parentElement.querySelectorAll('.c-kokuchi-question__button');
+    buttonsInQuestion.forEach((button) => button.classList.remove('c-kokuchi-question__button--selected'));
+    clickedButton.classList.add('c-kokuchi-question__button--selected');
   };
 
   const handleButtonClick = (event) => {
@@ -66,42 +67,46 @@ const initializeKokuchiFlow = () => {
       currentStep1Answer = answer;
       resetDisplay();
       if (answer === 'はい') {
-        pattern1.style.display = 'block';
+        pattern1.classList.add('c-kokuchi-pattern--current');
       } else {
-        pattern2.style.display = 'block';
+        pattern2.classList.add('c-kokuchi-pattern--current');
       }
       const step2Buttons = document.querySelectorAll('[data-step="2"]');
-      step2Buttons.forEach((button) => button.classList.remove('active')); // 2問目のボタンからactiveを外す
+      step2Buttons.forEach((button) => button.classList.remove('c-kokuchi-question__button--selected'));
     } else if (step === '2') {
       // 2問目
       currentStep2Answer = answer;
       resetDisplay();
       if (answer === 'はい') {
-        pattern3.style.display = 'block';
+        pattern3.classList.add('c-kokuchi-pattern--current');
       } else {
-        pattern4.style.display = 'block';
+        pattern4.classList.add('c-kokuchi-pattern--current');
       }
-      pattern2.style.display = 'block';
+      pattern2.classList.add('c-kokuchi-pattern--current');
     }
   };
 
   buttons.forEach((button) => button.addEventListener('click', (e) => handleButtonClick(e)));
 };
 
-window.addEventListener('load', function () {
-  const mvElement = document.querySelectorAll(`
-    .section-hero__titleline-pop,
-    .section-hero__titleline-pop-elm,
-    .section-hero__titleline-title,
-    .section-hero__titleline-title-flash-elm,
-    .section-hero__titleline-title-flash-elm--1,
-    .section-hero__titleline-title-flash-elm--2,
-    .section-hero__titleline-title-flash-elm--3,
-    .section-hero__titleline-title-flash-elm-img
-  `);
-  mvElement.forEach(function (element) {
-    element.classList.add('loaded');
+window.addEventListener('load', () => {
+  const mvElements = [
+    { selector: '.section-hero__title-pop', loadedClass: 'section-hero__title-pop--loaded' },
+    { selector: '.section-hero__title-pop-elm', loadedClass: 'section-hero__title-pop-elm--loaded' },
+    { selector: '.section-hero__title-main', loadedClass: 'section-hero__title-main--loaded' },
+    { selector: '.section-hero__title-main-flash--top', loadedClass: 'section-hero__title-main-flash--top--loaded' },
+    { selector: '.section-hero__title-main-flash--center', loadedClass: 'section-hero__title-main-flash--center--loaded' },
+    { selector: '.section-hero__title-main-flash--btm', loadedClass: 'section-hero__title-main-flash--btm--loaded' },
+    { selector: '.section-hero__title-main-flash-img', loadedClass: 'section-hero__titleline-title-flash-img--loaded' },
+  ];
+  
+  document.querySelectorAll(mvElements.map((item) => item.selector).join(',')).forEach((element) => {
+    const matchingItem = mvElements.find((item) => element.matches(item.selector));
+    if (matchingItem) {
+      element.classList.add(matchingItem.loadedClass);
+    }
   });
+
   setSlider();
   triggerAnimation();
   setInterval(triggerAnimation, 5000);
